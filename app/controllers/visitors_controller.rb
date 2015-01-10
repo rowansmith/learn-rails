58d937cacd37dc946ba79b390437800e615b2038
Rowan <rowan.smith@cloudbi.co.za>
2015-01-10 16:09:06 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,9 @@
 class VisitorsController < ApplicationController
 
   def new
-    @owner = Owner.new
+    @owner         = Owner.new
+    flash[:notice] = 'Welcome!'
+    flash[:alert]  = 'My birthday is soon.' if @owner.countdown < 10
   end
 
 end
